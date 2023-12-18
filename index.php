0ef9655ee7e5451a89310ca37011f7c90d19400c
ThomasVentu <ventuthomas@gmail.com>
2023-12-18 10:27:14 +0100

    Base

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Projet</title>
+  </head>
+  <body>
+    <titre>Souvenir</titre>
+    <h1>Bonjour</h1>
+    <form action="index.php">
+        <label for="Prenom">Prénom :</label><br>
+        <input type="text" id="fname" name="fname" value="Prénom"><br>
+        <label for="Nom">Nom :</label><br>
+        <input type="text" id="lname" name="lname" value="Nom"><br><br>
+        <input type="Co1" value="Submit">
+    </form> 
+  </body>
+</html>

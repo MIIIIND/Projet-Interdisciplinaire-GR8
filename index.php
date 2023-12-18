f841c4bda4a86bb48f41e977e7689cf44140ee6c
ThomasVentu <ventuthomas@gmail.com>
2023-12-18 10:32:29 +0100

    schéma de Base

--- index.php
+++ index.php
@@ -3,17 +3,12 @@
   <head>
     <meta charset="UTF-8" />
     <link rel="stylesheet" href="style.css" />
-    <title>Projet</title>
+    <title>ActionParc</title>
   </head>
+  <header>
+  </header>
   <body>
-    <titre>Souvenir</titre>
-    <h1>Bonjour</h1>
-    <form action="index.php">
-        <label for="Prenom">Prénom :</label><br>
-        <input type="text" id="fname" name="fname" value="Prénom"><br>
-        <label for="Nom">Nom :</label><br>
-        <input type="text" id="lname" name="lname" value="Nom"><br><br>
-        <input type="Co1" value="Submit">
-    </form> 
   </body>
+  <footer>
+  </footer>
 </html>

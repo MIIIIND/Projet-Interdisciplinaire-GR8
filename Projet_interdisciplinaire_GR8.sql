eac9976102fdbcc396d2137a12b86a0d806ee552
Ioka <clearseasc@gmail.com>
2023-12-19 13:56:28 +0100

    update DB

--- Projet_interdisciplinaire_GR8.sql
+++ Projet_interdisciplinaire_GR8.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `role` (
-  `role_id` INT PRIMARY KEY,
+  `role_id` INT PRIMARY KEY AUTO_INCREMENT,
   `role_name` VARCHAR(255)
 );
 
 CREATE TABLE `user` (
-  `user_id` INT PRIMARY KEY,
+  `user_id` INT PRIMARY KEY AUTO_INCREMENT,
   `first_name` VARCHAR(255),
   `second_name` VARCHAR(255),
   `role_Fk` INT,
@@ -16,17 +16,17 @@ CREATE TABLE `user` (
 );
 
 CREATE TABLE `product_type` (
-  `product_type_id` INT PRIMARY KEY,
+  `product_type_id` INT PRIMARY KEY AUTO_INCREMENT,
   `type_name` VARCHAR(255)
 );
 
 CREATE TABLE `shop_type` (
-  `shop_type_id` INT PRIMARY KEY,
+  `shop_type_id` INT PRIMARY KEY AUTO_INCREMENT,
   `type_name` VARCHAR(255)
 );
 
 CREATE TABLE `shop` (
-  `shop_id` INT PRIMARY KEY,
+  `shop_id` INT PRIMARY KEY AUTO_INCREMENT,
   `shop_name` VARCHAR(255),
   `shop_location` VARCHAR(255),
   `manager_user_id_Fk` INT,
@@ -38,7 +38,7 @@ CREATE TABLE `shop` (
 );
 
 CREATE TABLE `product` (
-  `product_id` INT PRIMARY KEY,
+  `product_id` INT PRIMARY KEY AUTO_INCREMENT,
   `Souvenir_name` VARCHAR(255),
   `price` FLOAT,
   `shop_id_Fk` INT,
@@ -50,12 +50,12 @@ CREATE TABLE `product` (
 );
 
 CREATE TABLE `order_state` (
-  `order_state_id` INT PRIMARY KEY,
+  `order_state_id` INT PRIMARY KEY AUTO_INCREMENT,
   `state_name` VARCHAR(255)
 );
 
 CREATE TABLE `order` (
-  `order_id` INT PRIMARY KEY,
+  `order_id` INT PRIMARY KEY AUTO_INCREMENT,
   `product_Fk` INT,
   `client_user_id_Fk` INT,
   `quantity` INT,
@@ -67,13 +67,13 @@ CREATE TABLE `order` (
 );
 
 CREATE TABLE `cottage` (
-  `cottage_id` INT PRIMARY KEY,
+  `cottage_id` INT PRIMARY KEY AUTO_INCREMENT,
   `name` VARCHAR(255),
   `location` VARCHAR(255)
 );
 
 CREATE TABLE `comment` (
-  `comment_id` INT PRIMARY KEY,
+  `comment_id` INT PRIMARY KEY AUTO_INCREMENT,
   `author_user_id_Fk` INT,
   `target_shop_Fk` INT,
   `content` TEXT,
@@ -83,7 +83,7 @@ CREATE TABLE `comment` (
 );
 
 CREATE TABLE `message` (
-  `message_id` INT PRIMARY KEY,
+  `message_id` INT PRIMARY KEY AUTO_INCREMENT,
   `from_user_Fk` INT,
   `to_user_Fk` INT,
   `concerning_shop_Fk` INT,

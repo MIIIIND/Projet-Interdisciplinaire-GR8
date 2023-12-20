ee27dc14127cfd9c3b273023dc9020aff4bd1620
Marcin KULESZA <marcin.kulesza@std.heh.be>
2023-12-20 14:45:49 +0100

    fully functional php admin panel needs links

--- admin/admin.php
+++ admin/admin.php
@@ -5,27 +5,22 @@ include 'db.php';
 $nomOptions = '';
 $typeOptions = '';
 
-
-// --------------------------------------------------------
-//                 MODIFICATION MAGASIN
-// --------------------------------------------------------
-
 // Fetch options for Nom dropdown
 $nomQuery = "SELECT shop_name FROM shop";
 $nomResult = $bd->query($nomQuery);
-while ($row = $nomResult->fetch(PDO::FETCH_ASSOC)) { // Corrected to fetch(PDO::FETCH_ASSOC)
+while ($row = $nomResult->fetch(PDO::FETCH_ASSOC)) {
     $nomOptions .= "<option value='" . htmlspecialchars($row['shop_name']) . "'>" . htmlspecialchars($row['shop_name']) . "</option>";
 }
 
 // Fetch options for Type dropdown
 $typeQuery = "SELECT type_name FROM shop_type";
 $typeResult = $bd->query($typeQuery);
-while ($row = $typeResult->fetch(PDO::FETCH_ASSOC)) { // Corrected to fetch(PDO::FETCH_ASSOC)
+while ($row = $typeResult->fetch(PDO::FETCH_ASSOC)) {
     $typeOptions .= "<option value='" . htmlspecialchars($row['type_name']) . "'>" . htmlspecialchars($row['type_name']) . "</option>";
 }
 
-// Check if the form has been submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+// Check if the Modification Form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['modifier'])) {
     $selectedNom = $_POST['nom'];
     $nomTextBox = $_POST['nomTextBox'];
     $selectedType = $_POST['type'];
@@ -33,23 +28,85 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // SQL to update the shop table
     $updateQuery = "UPDATE shop SET shop_name = ?, shop_type_Fk = (SELECT shop_type_id FROM shop_type WHERE type_name = ?) WHERE shop_name = ?";
 
-    // Prepare and execute query (Prevents SQL Injection)
+    // Prepare and execute query
     $stmt = $bd->prepare($updateQuery);
     $stmt->bindParam(1, $nomTextBox);
     $stmt->bindParam(2, $selectedType);
     $stmt->bindParam(3, $selectedNom);
     if ($stmt->execute()) {
-        // Refresh the page after successful update
         header("Location: ".$_SERVER['PHP_SELF']);
         exit();
     } else {
         echo "<p>Error updating record: " . htmlspecialchars($bd->errorInfo()[2]) . "</p>";
     }
+}
 
+// Check if the Ajout Form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['ajout'])) {
+    $nomMagasin = $_POST['nomMagasin'];
+    $typeMagasin = $_POST['typeMagasin'];
+
+    // SQL to insert a new shop
+    $insertQuery = "INSERT INTO shop (shop_name, shop_type_Fk) VALUES (?, (SELECT shop_type_id FROM shop_type WHERE type_name = ?))";
+
+    // Prepare and execute query
+    $stmt = $bd->prepare($insertQuery);
+    $stmt->bindParam(1, $nomMagasin);
+    $stmt->bindParam(2, $typeMagasin);
+    if ($stmt->execute()) {
+        header("Location: ".$_SERVER['PHP_SELF']);
+        exit();
+    } else {
+        echo "<p>Error adding new shop: " . htmlspecialchars($bd->errorInfo()[2]) . "</p>";
+    }
     
 }
-?>
 
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['ajoutType'])) {
+    $typeMagasin = $_POST['AjoutTypeMagasin'];
+
+    // SQL to insert a new shop type
+    $insertTypeQuery = "INSERT INTO shop_type (type_name) VALUES (?)";
+
+    // Prepare and execute query
+    $stmt = $bd->prepare($insertTypeQuery);
+    $stmt->bindParam(1, $typeMagasin);
+    if ($stmt->execute()) {
+        header("Location: ".$_SERVER['PHP_SELF']);
+        exit();
+    } else {
+        echo "<p>Error adding new shop type: " . htmlspecialchars($bd->errorInfo()[2]) . "</p>";
+    }
+}
+
+
+
+// Fetch options for Type to Delete dropdown
+$typeDeleteOptions = '';
+$typeDeleteQuery = "SELECT type_name FROM shop_type";
+$typeDeleteResult = $bd->query($typeDeleteQuery);
+while ($row = $typeDeleteResult->fetch(PDO::FETCH_ASSOC)) {
+    $typeDeleteOptions .= "<option value='" . htmlspecialchars($row['type_name']) . "'>" . htmlspecialchars($row['type_name']) . "</option>";
+}
+
+// Check if the Suppression Form has been submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['supprimer'])) {
+    $typeToDelete = $_POST['typeToDelete'];
+
+    // SQL to delete the selected shop type
+    $deleteQuery = "DELETE FROM shop_type WHERE type_name = ?";
+
+    // Prepare and execute query
+    $stmt = $bd->prepare($deleteQuery);
+    $stmt->bindParam(1, $typeToDelete);
+    if ($stmt->execute()) {
+        header("Location: ".$_SERVER['PHP_SELF']);
+        exit();
+    } else {
+        echo "<p>Error deleting shop type: " . htmlspecialchars($bd->errorInfo()[2]) . "</p>";
+    }
+}
+?>
 
 
 
@@ -123,6 +180,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <body>
 
     <div class="big-box">
+        <!-- Modification Form -->
     <form class="small-box" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <div>Modification Magasin</div>
 
@@ -150,19 +208,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
 
         <div class="button-container">
-            <input type="submit" class="centered-button" value="Modifier">
+        <input type="submit" class="centered-button" name="modifier" value="Modifier">
         </div>
     </form>
     
-        <form class="small-box" action="/submit-url" method="post">
-            <div>Ajout Type</div>
-            <div>
-                <label for="typeTextBox">Type</label>
-                <input type="text" id="typeTextBox" />
-            </div>
-            <div class="button-container">
-                <input type="submit" class="centered-button" value="Ajout">
-            </div>
+    <!-- Ajout Magasin Form -->
+        <form class="small-box" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+        <div>Ajout de Magasin</div>
+        <div>
+            <label for="nomMagasin">Nom</label>
+            <input type="text" id="nomMagasin" name="nomMagasin" />
+        </div>
+        <div>
+            <label for="typeMagasin">Type</label>
+            <select id="typeMagasin" name="typeMagasin">
+                <?php echo $typeOptions; ?>
+            </select>
+        </div>
+        <div class="button-container">
+            <input type="submit" class="centered-button" name="ajout" value="Ajout">
+        </div>
         </form>
     
         <form class="small-box" action="/submit-url" method="post">
@@ -170,36 +235,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <input type="submit" class="centered-button" value="Gestion Moderateur">
             </div>
         </form>
+
+        <!-- Ajout Type de magasin Form -->
+  <!-- ======================================= -->
+        
     
-        <form class="small-box" action="/submit-url" method="post">
-            <div>Ajout de Magasin</div>
+        <form class="small-box" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+            <div>Ajout de type de magasin</div>
             <div>
-                <label for="nomMagasin">Nom</label>
-                <input type="text" id="nomMagasin" />
-            </div>
-            <div>
-                <label for="typeMagasin">Type</label>
-                <select id="typeMagasin">
-                    <option value="type1">Type 1</option>
-                    <option value="type2">Type 2</option>
-                </select>
+                <label for="AjoutTypeMagasin">Nom</label>
+                <input type="text" id="AjoutTypeMagasin" name="AjoutTypeMagasin" />
             </div>
             <div class="button-container">
-                <input type="submit" class="centered-button" value="Ajout">
+                <input type="submit" class="centered-button" name="ajoutType" value="Ajout">
             </div>
         </form>
-    
-        <form class="small-box" action="/submit-url" method="post">
-            <div>Suppression Type</div>
+
+
+        <form class="small-box" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+            <div>Suppression de Type de magasin</div>
             <div>
                 <label for="typeToDelete">Type</label>
-                <select id="typeToDelete">
-                    <option value="type1">Type 1</option>
-                    <option value="type2">Type 2</option>
+                <select id="typeToDelete" name="typeToDelete">
+                    <?php echo $typeDeleteOptions; ?>
                 </select>
             </div>
             <div class="button-container">
-                <input type="submit" class="centered-button" value="Supprimer">
+                <input type="submit" class="centered-button" name="supprimer" value="Supprimer">
             </div>
         </form>
     

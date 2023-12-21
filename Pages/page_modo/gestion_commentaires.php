eb43187fe958350bfd02bb8f3e461944bfee919f
Ioka <clearseasc@gmail.com>
2023-12-21 15:57:45 +0100

    modo wip

--- Pages/page_modo/gestion_commentaires.php
+++ Pages/page_modo/gestion_commentaires.php
@@ -1,32 +1,16 @@
 <?php
-date_default_timezone_set('Europe/Brussels');
-$hote = 'localhost';
-$nomBD = 'isim_parc';
-$user = 'root';
-$mdp = '';
+require 'm-Comment.php';
+$COMMENT= new Comment();
 
-try {
-    $bd = new PDO('mysql:host=' . $hote . ';dbname=' . $nomBD, $user, $mdp);
-    $bd->exec("SET NAMES 'utf8'");
-} catch (Exception $e) {
-    echo "Erreur de connexion à la BD : $e";
-    exit();
-}
+$shop_id = $_SESSION['shop_id'] = 4;
 
-$shop_id = 4;
-
-$cater = $bd->prepare("SELECT comment.content, comment.score,comment.comment_id ,comment.author_user_id_Fk, user.first_name, user.second_name FROM comment JOIN user ON comment.author_user_id_Fk = user.user_id WHERE comment.target_shop_Fk=?;");
-$cater->execute(array($shop_id));
+$cater=$COMMENT->getShopComments($shop_id);
 
 if (isset($_POST['supprimer'])) {
-  $commend_id = $_POST['comment_id'];
-  $requeteDeleteCommentaires = $bd->prepare("DELETE FROM comment WHERE comment_id = ?");
-  $requeteDeleteCommentaires->execute(array((int) $commend_id));
+  $COMMENT->delComment($_POST['comment_id']);
   header("Location: gestion_commentaires.php");
   exit();
 }
-
-
 ?>
 
 <!doctype html>
@@ -42,9 +26,9 @@ if (isset($_POST['supprimer'])) {
 	<a href="modo.php">Retour</a>
 	<?php
 	while ($data = $cater->fetch()) {
-    echo '<p>', $data['first_name']. ' '. $data['second_name']. ' : '.$data['score'], '/5', '</p><p>', $data['content'], '</p>
+    echo '<p>', $data->first_name. ' '. $data->second_name. ' : '.$data->score, '/5', '</p><p>', $data->content, '</p>
     <form method="post">
-		<input type="hidden" name="comment_id" value="' . $data['comment_id'] . '">
+		<input type="hidden" name="comment_id" value="' . $data->comment_id . '">
         <input type="submit" name="supprimer" value="supprimer">
     </form>';
 	}

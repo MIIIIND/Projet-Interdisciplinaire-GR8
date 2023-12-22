38c1f6f661bab7f2b7db943ff2067126d98fc777
Marcin KULESZA <marcin.kulesza@std.heh.be>
2023-12-22 10:58:22 +0100

    Insert message fonctionne mais que user side (pas admin)

--- message.php
+++ message.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Messaging Page</title>
     <style>
-        /* Add your CSS styles here */
+        /* CSS styles */
         body {
             font-family: Arial, sans-serif;
         }
@@ -22,20 +22,40 @@
 include 'db.php';
 
 // Define the concerning_shop and userID
-$concerning_shop = 1; // This will be dynamic in the future
+$concerning_shop = 1; // Assuming 'fleurs' corresponds to shop_id 1
 $userID = 1; // This will be dynamic in the future
 
 // Handle new message submission
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['newMessage'])) {
     $newMessage = $_POST['newMessage'];
-    // Insert the new message into the database
-    $insertSQL = "INSERT INTO message (from_user_Fk, concerning_shop_Fk, content) VALUES (:userID, :concerning_shop, :content)";
-    $insertStmt = $bd->prepare($insertSQL);
-    $insertStmt->execute(['userID' => $userID, 'concerning_shop' => $concerning_shop, 'content' => $newMessage]);
 
-    // Refresh the page
-    header("Location: " . $_SERVER['PHP_SELF']);
-    exit;
+    // First, get the user_id of the shop manager for the given shop
+    $getManagerIdSQL = "SELECT manager_user_id_Fk FROM shop WHERE shop_id = :shopId";
+    $getManagerIdStmt = $bd->prepare($getManagerIdSQL);
+    $getManagerIdStmt->execute(['shopId' => $concerning_shop]);
+    $manager = $getManagerIdStmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($manager && isset($manager['manager_user_id_Fk'])) {
+        $managerUserId = $manager['manager_user_id_Fk'];
+
+        // Insert the new message into the database
+        $insertSQL = "INSERT INTO message (from_user_Fk, to_user_Fk, concerning_shop_Fk, content) 
+                      VALUES (:fromUserID, :toUserID, :concerning_shop, :content)";
+        $insertStmt = $bd->prepare($insertSQL);
+        $insertStmt->execute([
+            'fromUserID' => $userID,
+            'toUserID' => $managerUserId,
+            'concerning_shop' => $concerning_shop,
+            'content' => $newMessage
+        ]);
+
+        // Refresh the page
+        header("Location: " . $_SERVER['PHP_SELF']);
+        exit;
+    } else {
+        echo "Manager not found for the shop with ID: $concerning_shop";
+        exit;
+    }
 }
 
 // SQL to fetch messages
@@ -55,7 +75,6 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     echo htmlspecialchars($row['content']);
     echo "</div>";
 }
-
 ?>
 
 <!-- Message submission form -->
